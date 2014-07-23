c351532ae13c75ef7545ee64e3ed9ff6c8dad1f1
unknown <ameysgitrepository>
2014-07-23 13:17:35 +0530

    added src for login on 23 July by Amey

--- src/com/fission/controller/UserLoginController.java
+++ src/com/fission/controller/UserLoginController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
